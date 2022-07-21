00939a17c8b15cac6800b62973296131918e85e5
Salman Ahmed <salmanahmedx@hotmail.com>
2022-07-21 22:52:48 +0600

    Conditional rendering based on state

--- src/search.js
+++ src/search.js
@@ -4,12 +4,14 @@ import { useSelector, useDispatch } from "react-redux";
 
 const Search = () => {
     const counter = useSelector((state) => state.counter);
+    const signedIn = useSelector((state) => state.isLogged)
     const dispatch = useDispatch();
     return (
         <div>
             <input type="text" />
             <h1>Hey {counter}</h1>
             <button onClick={() => dispatch({ type: "SIGN_IN" })}>SIGN IN</button>
+            {signedIn && <h1>Movie List</h1>}
         </div>
     );
 };
